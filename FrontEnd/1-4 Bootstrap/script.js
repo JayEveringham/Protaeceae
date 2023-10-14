cd49b86ddd3306831ea707d4ab10c7caca32c4eb
JayEveringham <jay.e@posteo.net>
2023-10-15 05:52:19 +1000

    Initial commit

---
+++ FrontEnd/1-4 Bootstrap/script.js
@@ -0,0 +1,14 @@
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            const animatedLeft = document.querySelectorAll(".animatedleft");
+            animatedLeft.forEach(box => box.classList.add("fadeInLeft"));
+            const animatedRight = document.querySelectorAll(".animatedright");
+            animatedRight.forEach(box => box.classList.add("fadeInRight"));
+            const animatedFade = document.querySelectorAll(".animatedfade");
+            animatedFade.forEach(box => box.classList.add("fadeIn"));
+        }
+    })
+})
+
+observer.observe(document.querySelector(".container2"));
