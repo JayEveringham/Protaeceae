597d8aa3076755beaba4caf3bde4a8d4de592054
JayEveringham <jay.e@posteo.net>
2024-02-24 07:23:23 +1000

    Working quantity buttons on profile page.  Profile icon in nav.

--- FrontEnd/Protaeceae/quantityButton.js
+++ FrontEnd/Protaeceae/quantityButton.js
@@ -1,35 +1,37 @@
-// Quantity button functionality
-
 document.addEventListener("DOMContentLoaded", () => {
-  const minusButton = document.querySelector(".btn-quantity-minus");
-  const plusButton = document.querySelector(".btn-quantity-plus");
-  const quantityDisplay = document.querySelector(".quantity");
+  const minusButtons = document.querySelectorAll(".btn-quantity-minus");
+  const plusButtons = document.querySelectorAll(".btn-quantity-plus");
+  const quantityDisplays = document.querySelectorAll(".quantity");
 
-  function updateMinusButtonState() {
+  function updateMinusButtonState(quantityDisplay, minusButton) {
     const quantity = parseInt(quantityDisplay.textContent, 10);
-    if (quantity <= 1) {
+    if (quantity < 1) {
       minusButton.classList.add("disabled");
     } else {
       minusButton.classList.remove("disabled");
     }
   }
 
-  // Initial state update for minus button
-  updateMinusButtonState();
+  minusButtons.forEach((minusButton, index) => {
+    // Initial state update for minus button
+    updateMinusButtonState(quantityDisplays[index], minusButton);
 
-  minusButton.addEventListener("click", (event) => {
-    event.preventDefault(); // Prevent default anchor behavior
-    let quantity = parseInt(quantityDisplay.textContent, 10);
-    if (quantity > 1) {
-      quantityDisplay.textContent = quantity - 1;
-    }
-    updateMinusButtonState();
+    minusButton.addEventListener("click", (event) => {
+      event.preventDefault(); // Prevent default anchor behavior
+      let quantity = parseInt(quantityDisplays[index].textContent, 10);
+      if (quantity > 0) {
+        quantityDisplays[index].textContent = quantity - 1;
+      }
+      updateMinusButtonState(quantityDisplays[index], minusButton);
+    });
   });
 
-  plusButton.addEventListener("click", (event) => {
-    event.preventDefault(); // Prevent default anchor behavior
-    let quantity = parseInt(quantityDisplay.textContent, 10);
-    quantityDisplay.textContent = quantity + 1;
-    updateMinusButtonState();
+  plusButtons.forEach((plusButton, index) => {
+    plusButton.addEventListener("click", (event) => {
+      event.preventDefault(); // Prevent default anchor behavior
+      let quantity = parseInt(quantityDisplays[index].textContent, 10);
+      quantityDisplays[index].textContent = quantity + 1;
+      updateMinusButtonState(quantityDisplays[index], minusButtons[index]);
+    });
   });
 });
