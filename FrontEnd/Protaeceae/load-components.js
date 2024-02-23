5984f5dee8f7044d8193d0ca8b8bae30c82d6b02
JayEveringham <jay.e@posteo.net>
2024-02-24 04:33:20 +1000

    Footer integrated.  load-components.js disabled.

--- FrontEnd/Protaeceae/load-components.js
+++ FrontEnd/Protaeceae/load-components.js
@@ -1,9 +1,9 @@
-async function loadComponent(componentPath, placeholderId) {
-  const response = await fetch(componentPath);
-  const html = await response.text();
-  document.getElementById(placeholderId).innerHTML = html;
-}
+// async function loadComponent(componentPath, placeholderId) {
+//   const response = await fetch(componentPath);
+//   const html = await response.text();
+//   document.getElementById(placeholderId).innerHTML = html;
+// }
 
-document.addEventListener("DOMContentLoaded", () => {
-  loadComponent("footer.html", "footer-placeholder");
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//   loadComponent("footer.html", "footer-placeholder");
+// });
